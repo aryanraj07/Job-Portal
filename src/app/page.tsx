34ceca477331b19f10b958a4b1d7411b658c59fd
aryanraj07 <aryanraj010010@gmail.com>
2026-06-20 02:15:27 +0530

    made chnages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,5 @@ import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  const clerkUser = await currentUser();
-
-  if (clerkUser) {
-    const user = await prisma.user.upsert({
-      where: {
-        clerkId: clerkUser.id,
-      },
-      update: {},
-      create: {
-        clerkId: clerkUser.id,
-        email: clerkUser.emailAddresses[0]?.emailAddress ?? "",
-        name: `${clerkUser.firstName ?? ""} ${clerkUser.lastName ?? ""}`,
-      },
-    });
-
-    console.log("UPSERTED USER", user);
-  }
-
-  const users = await prisma.user.findMany();
-
-  console.log("ALL USERS", users);
-
   return <div>Home</div>;
 }
--- src/components/common/UserSync.tsx
+++ src/components/common/UserSync.tsx
@@ -5,12 +5,14 @@ import { useEffect } from "react";
 
 export default function UserSync() {
   const { isLoaded, isSignedIn } = useUser();
+
   useEffect(() => {
     if (!isLoaded || !isSignedIn) return;
+
     fetch("/api/me/sync", {
       method: "POST",
     });
-  }, []);
+  }, [isLoaded, isSignedIn]);
 
   return null;
 }
