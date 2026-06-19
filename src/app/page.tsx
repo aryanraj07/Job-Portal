99bff4f72dfd023b523c99cb7290caf0e2126726
aryanraj07 <aryanraj010010@gmail.com>
2026-06-20 01:47:38 +0530

    made changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
   const users = await prisma.user.findMany();
   console.log("users", users);
+
+  const clerkUser = await currentUser();
+
+  console.log("CLERK USER:", {
+    id: clerkUser?.id,
+    email: clerkUser?.emailAddresses?.[0]?.emailAddress,
+    firstName: clerkUser?.firstName,
+    lastName: clerkUser?.lastName,
+  });
 
   return <></>;
 }
