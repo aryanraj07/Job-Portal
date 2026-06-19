aee830e873c3cf92c3409a5e4a876c4dce67969b
aryanraj07 <aryanraj010010@gmail.com>
2026-06-19 23:49:10 +0530

    made chnges for testing pg

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,24 @@
 import { PrismaClient } from "../../generated/prisma/client";
 
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 //
-const adapter = new PrismaPg({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+
+  ssl: {
+    ca: process.env.AIVEN_CA_CERT,
+    rejectUnauthorized: true,
+  },
 });
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+// });
+const adapter = new PrismaPg(pool);
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
